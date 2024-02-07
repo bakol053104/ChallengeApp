c53aae94115fd579837500effd7eee73e32ad0dd
bakol053104 <bakol@tlen.pl>
2024-02-07 22:27:39 +0100

    Zadanie dzień-4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, działanie z GITEM");
+﻿var name = "Ewa";
+var age = 11;
+char sex = 'K'; // K- kobieta, M-mężczyzna
+
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else
+{
+
+    if (sex == 'K')
+    {
+        if (age < 30)
+        {
+            Console.WriteLine("Kobieta poniżej 30 lat");
+        }
+
+    }
+    else
+    {
+        if (age < 18)
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+        }
+    }
+}
+
+
+
+
