f10b549b90d8d3247bb738b7af79f989a6e6ebb8
bakol053104 <bakol@tlen.pl>
2024-02-08 17:22:25 +0100

    Zadanie dzien  4- v2

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,30 +1,22 @@
 ﻿var name = "Ewa";
 var age = 11;
-char sex = 'K'; // K- kobieta, M-mężczyzna
+var sex = 'M'; // K- kobieta, M- mężczyzna
 
 if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else
+else if (sex == 'K')
 {
-
-    if (sex == 'K')
-    {
-        if (age < 30)
-        {
-            Console.WriteLine("Kobieta poniżej 30 lat");
-        }
-    }
-    else
-    {
-        if (age < 18)
-        {
-            Console.WriteLine("Niepełnoletni mężczyzna");
-        }
-    }
+            if (age < 30)
+            {
+                Console.WriteLine("Kobieta poniżej 30 lat");
+            }     
 }
-
+    else if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
 
 
 
