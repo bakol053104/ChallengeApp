54fc02b9fea9b52d9cd803df8bce5c9878db330e
bakol053104 <bakol@tlen.pl>
2024-02-09 22:56:57 +0100

    Zadanie dzień- 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,16 @@
 ﻿var number = 1223445667778888990;
 var numberInString = number.ToString();
 var letters = numberInString.ToArray();
-var CountersDigit = new int[10];
+var countersDigit = new int[10];
 
 foreach (var digit in letters)
 {
-    CountersDigit[digit - 48]++; // minus nr znaku cyfry 0 w tablicy ASCII
+    countersDigit[digit - 48]++; // minus nr znaku cyfry 0 w tablicy ASCII
 }
 
 Console.WriteLine($"{number}\n");
 
-for (var i=0; i<CountersDigit.Length; i++)
+for (var i=0; i<countersDigit.Length; i++)
 {
-    Console.WriteLine($"{i}=> {CountersDigit[i]}");
+    Console.WriteLine($"{i}=> {countersDigit[i]}");
 }
