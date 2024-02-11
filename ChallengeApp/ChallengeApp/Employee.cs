bc67dde8034dca1eb3a98158a10891fb3830bbf4
bakol053104 <bakol@tlen.pl>
2024-02-11 18:50:32 +0100

    Zadanie dzień- 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace ChallengeApp
+{
+    class Employee
+    {
+        private List<int> grades = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.grades.Sum();
+            }
+        }
+
+        public void AddGrade(int grade)
+        {
+            this.grades.Add(grade);
+        }
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,46 @@
-﻿var number = 1223445667778888990;
-var numberInString = number.ToString();
-var letters = numberInString.ToArray();
-var countersDigit = new int[10];
+﻿using ChallengeApp;
 
-foreach (var digit in letters)
+Employee employee1 = new Employee("Imie_1", "Nazwisko_1", 21);
+Employee employee2 = new Employee("Imie_2", "Nazwisko_2", 22);
+Employee employee3 = new Employee("Imie_3", "Nazwisko_3", 23);
+
+List<Employee> employees = new List<Employee>();
+
+employee1.AddGrade(1);
+employee1.AddGrade(5);
+employee1.AddGrade(7);
+employee1.AddGrade(6);
+employee1.AddGrade(9);
+Console.WriteLine($"{employee1.Name} {employee1.Surname} Wiek: {employee1.Age} Ocena: {employee1.Result} ");
+
+employee2.AddGrade(2);
+employee2.AddGrade(9);
+employee2.AddGrade(8);
+employee2.AddGrade(7);
+employee2.AddGrade(6);
+Console.WriteLine($"{employee2.Name} {employee2.Surname} Wiek: {employee2.Age} Ocena: {employee2.Result} ");
+
+employee3.AddGrade(3);
+employee3.AddGrade(4);
+employee3.AddGrade(6);
+employee3.AddGrade(9);
+employee3.AddGrade(3);
+Console.WriteLine($"{employee3.Name} {employee3.Surname} Wiek: {employee3.Age} Ocena: {employee3.Result} ");
+
+employees.Add(employee1);
+employees.Add(employee2);
+employees.Add(employee3);
+
+var maxResultTemp = -1;
+Employee employeeMaxResultData = null;
+
+foreach (Employee emp in employees)
 {
-    countersDigit[digit - 48]++; // minus nr znaku cyfry 0 w tablicy ASCII
-}
+    if (emp.Result > maxResultTemp)
+    {
+        maxResultTemp = emp.Result;
+        employeeMaxResultData = emp;
+    }
 
-Console.WriteLine($"{number}\n");
-
-for (var i=0; i<countersDigit.Length; i++)
-{
-    Console.WriteLine($"{i}=> {countersDigit[i]}");
 }
+Console.WriteLine($"\nWynik: \n\n{employeeMaxResultData.Name} {employeeMaxResultData.Surname} Wiek: {employeeMaxResultData.Age} Ocena: {employeeMaxResultData.Result} ");
